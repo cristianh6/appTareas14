852bd4305324276e8da01d8046af2714e9845438
cristianh6 <cristian.casalaspro@gmail.com>
2022-02-16 10:31:49 -0300

    agrega controladores

---
+++ git.js
@@ -0,0 +1,18 @@
+/* 
+
+Recuento de comandos
+
+- git --version  = Conocer la version de git
+
+- git init // Inicializar un repositorio de git (una sola vez por proyecto)
+
+- git status // Conocer el estado de git
+- git add . // Agregar archivos al seguimiento de git
+- git commit -m"Mensaje del commit" // Guardar archivos de seguimiento en git
+
+
+ CONFIGURACIONES
+- git config --global user.name "Nombre de usuario de github"
+- git config -- global user.email "Email con el que te logeaste en github" 
+
+*/
--- sombraj.js
+++ sombraj.js
@@ -1,9 +1,10 @@
 function noParesDeContarImparesHasta(numero){
     for(let i = 0; i <= numero; i++){
         if(i % 2 !== 0){
-            return [i];
+
+            }
+
         }
     }
-}
 
 console.log(noParesDeContarImparesHasta(10))
